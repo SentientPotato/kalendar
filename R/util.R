d47503707780d6a7ec938542b1d6c9b31ac6e5ef
Sentient Potato <sentient.potato138@gmail.com>
2023-07-09 12:14:29 -0500

    Add functions to find the start of the year

--- R/util.R
+++ R/util.R
@@ -1,16 +1,41 @@
-## Function for finding the Sunday before a given date
-previous_sunday = function(x) {
-    ## Ensure we're working with a Date object
+## Function to ensure we're working with a Date object
+ensure_date = function(x) {
     if ( !inherits(x, "Date") ) {
         x = try(as.Date(x), silent = TRUE)
         if ( inherits(x, "try-error") ) {
-            stop("previous_sunday() needs a date or an object coercible to one")
+            stop("Supply a date or an object coercible to one", call. = FALSE)
         }
     }
+    return(x)
+}
+
+## Function for finding the Sunday before a given date
+previous_sunday = function(x) {
+    ## Ensure we're working with a Date object
+    x = ensure_date(x)
     ## If the date is a Sunday, get the Sunday exactly one week before
     if ( lubridate::wday(x) == 1 ) {
         return(x - 7)
     }
     ## If not, use floor_date() to find the previous Sunday
     return(lubridate::floor_date(x, unit = "week", week_start = 7))
+}
+
+## Function to find the start of Advent in a (regular) calendar year
+get_Advent_start = function(year) {
+    return(previous_sunday(as.Date(paste0(year, "-12-25"))) - 21)
+}
+
+## Function to find the start of the liturgical year a date is in
+get_kalendar_start = function(x) {
+    ## Ensure we're working with a Date object
+    x = ensure_date(x)
+    ## Find the start of Advent in that (regular) calendar year
+    Advent_start = get_Advent_start(lubridate::year(x))
+    ## If the date x is after Advent_start, then that's what we need
+    if ( x >= Advent_start ) {
+        return(Advent_start)
+    }
+    ## Otherwise, we need the previous Advent start
+    return(get_Advent_start(lubridate::year(x) - 1))
 }
--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -1,4 +1,35 @@
-test_that("previous_sunday() catches Date errors", {
-    expect_error(previous_sunday("A"), ".*needs a date.*")
-    expect_error(previous_sunday(5), ".*needs a date.*")
+test_that("ensure_date() catches Date errors", {
+    expect_error(ensure_date("A"), "Supply a date.*")
+    expect_error(ensure_date(5), "Supply a date.*")
+})
+
+test_that("previous_sunday() works for Sundays", {
+    expect_equal(previous_sunday(as.Date("2023-07-09")), as.Date("2023-07-02"))
+})
+
+test_that("previous_sunday() works for non-Sundays", {
+    expect_equal(previous_sunday(as.Date("2023-07-10")), as.Date("2023-07-09"))
+    expect_equal(previous_sunday(as.Date("2023-07-15")), as.Date("2023-07-09"))
+})
+
+test_that("get_Advent_start() works", {
+    expect_equal(get_Advent_start(2022), as.Date("2022-11-27"))
+    expect_equal(get_Advent_start(2023), as.Date("2023-12-03"))
+    expect_equal(get_Advent_start(2024), as.Date("2024-12-01"))
+    expect_equal(get_Advent_start(2025), as.Date("2025-11-30"))
+})
+
+start2022 = as.Date("2022-11-27")
+start2023 = as.Date("2023-12-03")
+start2024 = as.Date("2024-12-01")
+test_that("get_kalendar_start() works for dates after Advent's beginning", {
+    expect_equal(get_kalendar_start(as.Date("2022-11-27")), start2022)
+    expect_equal(get_kalendar_start(as.Date("2022-11-30")), start2022)
+    expect_equal(get_kalendar_start(as.Date("2023-12-03")), start2023)
+    expect_equal(get_kalendar_start(as.Date("2023-12-10")), start2023)
+})
+
+test_that("get_kalendar_start() works for dates before Advent's beginning", {
+    expect_equal(get_kalendar_start(as.Date("2023-10-03")), start2022)
+    expect_equal(get_kalendar_start(as.Date("2025-10-30")), start2024)
 })
